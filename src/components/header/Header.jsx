ade43747e87153db2dadc733443b83150a1602db
Thiago Mendes Coscrato <thiago.mendes-e@hotmail.com>
2022-03-15 23:45:57 -0300

    .

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <Header>
+      <div className="container header__container">
+        <h5>Ola, eu sou o</h5>
+        <h1>Thiago Mendes</h1>
+        <h5 className="text-light">Desenvolvedor FrontEnd</h5>
+      </div>
+    </Header>
+  )
+}
+
+export default Header
