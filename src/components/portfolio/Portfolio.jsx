ff79dc08a95a146627ff68df4e50de73246b127d
Thiago Mendes Coscrato <thiago.mendes-e@hotmail.com>
2022-03-24 16:04:20 -0300

    portifolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Jogos em JS',
+    github: 'https://github.com/ThiagoMendes92/Jogos-JS',
+    demo: 'https://planetdefensejs-tm.netlify.app/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Form de Login',
+    github: 'https://github.com/ThiagoMendes92/Form-de-Login',
+    demo: 'https://loginscreen-tm.netlify.app/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Discord - Imersão React',
+    github: 'https://github.com/ThiagoMendes92/ImersaoReact-Alura',
+    demo: 'https://imersaoreact-discordalura-thiagomendes.vercel.app/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'DS Movie - Semana Spring React',
+    github: 'https://github.com/ThiagoMendes92/spring-react-DevSuperior',
+    demo: 'https://thiagomendesdsmovie.netlify.app/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Projeto google Glass - Curso HTML e CSS',
+    github: 'https://github.com/ThiagoMendes92/projeto-glass-html5',
+    demo: 'https://projeto-glass-html5-thiagomendes92.netlify.app/index.html'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Imersão Dev Alura',
+    github: 'https://github.com/ThiagoMendes92/ImersaoDev-Alura',
+    demo: 'https://github.com/ThiagoMendes92/ImersaoDev-Alura'
+  },
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,66 +59,22 @@ const Portfolio = () => {
       <h2>Portifólio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>Jogos em JS</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/ThiagoMendes92/Jogos-JS" className='btn' target='_blank'>Github</a>
-            <a href="https://planetdefensejs-tm.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className='btn' target='_blank'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className='btn' target='_blank'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className='btn' target='_blank'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className='btn' target='_blank'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className='btn' target='_blank'>Github</a>
-            <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
